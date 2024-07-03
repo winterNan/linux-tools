96d8c3463e3200958e0667a25c2c0f0f117e1a7d
Yuan Yao <yuan.yao@it.uu.se>
2024-07-03 17:45:56 +0200

    updated test.sh

--- perf/profiler/test/test.sh
+++ perf/profiler/test/test.sh
@@ -1,14 +1,19 @@
-#!/bin/sh
+#!/usr/bin
 
-PID=$(echo $$)
 CGROUP_DIR=/sys/fs/cgroup/perf_event/test/
 if [[ ! -d $CGROUP_DIR ]]
 then
     sudo mkdir -p $CGROUP_DIR
 fi
+
+PROGPATH=/home/yuan/Benchmarks/ascylib-vanilla/bin/
+PROG="lf-ll_harris -n 2 -i 16000000"
+
+$PROGPATH/${PROG} &
+
+PID=$!
 echo $PID | sudo tee $CGROUP_DIR/cgroup.procs
 
-btop &
-# sudo ../profiler ${PID} 1
-# sudo chown yuan:yuan report.html
-# mv report.html ../
+sudo ../profiler $PID 0
+sudo chown yuan:yuan report.html
+mv report.html ../
